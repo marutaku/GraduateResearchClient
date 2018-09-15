7c09285e1f697d35f5ca067fba02c22822c76170
marutaku <satouredstone@gmail.com>
2018-09-15 17:32:50 +0900

    Initial Commit

---
+++ GraduateResearchClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GraduateResearchClient
+//
+//  Created by 丸山拓己 on 2018/09/15.
+//  Copyright © 2018年 丸山拓己. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
